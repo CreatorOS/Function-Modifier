ed68a8a805cd2b16f71e54bf5c0d80802d724ecb
Sahil Vasava <sahilvasava@gmail.com>
2021-11-02 19:30:50 +0530

    removed code and added comment instructions

--- learn_src/contracts/FunctionModifier.sol
+++ learn_src/contracts/FunctionModifier.sol
@@ -15,20 +15,13 @@ contract FunctionModifier {
 
     // Modifier to check that the caller is the owner of
     // the contract.
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
-        // Underscore is a special character only used inside
-        // a function modifier and it tells Solidity to
-        // execute the rest of the code.
-        _;
-    }
+
+    // write onlyOwner modifier
 
     // Modifiers can take inputs. This modifier checks that the
     // address passed in is not the zero address.
-    modifier validAddress(address _addr) {
-        require(_addr != address(0), "Not valid address");
-        _;
-    }
+
+    // write validAddress modifier
 
     function changeOwner(address _newOwner) public onlyOwner validAddress(_newOwner) {
         owner = _newOwner;
@@ -37,13 +30,8 @@ contract FunctionModifier {
     // Modifiers can be called before and / or after a function.
     // This modifier prevents a function from being called while
     // it is still executing.
-    modifier noReentrancy() {
-        require(!locked, "No reentrancy");
 
-        locked = true;
-        _;
-        locked = false;
-    }
+    // write noReentrancy modifier
 
     function decrement(uint i) public noReentrancy {
         x -= i;
